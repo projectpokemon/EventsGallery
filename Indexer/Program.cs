ce1a29369fc990ceb5b94fdd7d1aea2ff469b3dc
Kamron Batman <kamron.batman@gmail.com>
2017-08-11 00:01:29 -0700

    Adds more Salazzle tests. Not sure if they are final.

---
+++ Indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
